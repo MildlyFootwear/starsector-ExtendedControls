4e10e1c1018e3a87d0a0dfa20910770e75ef3732
Shoey <shoeyfiller@gmail.com>
2024-08-28 07:38:11 -0400

    ditched robot for coreui nav

--- src/Shoey/ExtendedControls/EveryCampaignFrameScript.java
+++ src/Shoey/ExtendedControls/EveryCampaignFrameScript.java
@@ -4,13 +4,21 @@ import com.fs.starfarer.api.EveryFrameScript;
 import com.fs.starfarer.api.GameState;
 import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.campaign.CampaignUIAPI;
+import com.fs.starfarer.api.campaign.CoreUITabId;
+import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.SectorAPI;
+import com.fs.starfarer.api.campaign.rules.MemoryAPI;
+import com.fs.starfarer.api.impl.campaign.FleetInteractionDialogPluginImpl;
 import org.apache.log4j.Logger;
 import org.lwjgl.input.Keyboard;
 
+import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.util.*;
+import java.util.List;
 
 import static Shoey.ExtendedControls.MainPlugin.*;
 
@@ -20,6 +28,7 @@ public class EveryCampaignFrameScript implements EveryFrameScript {
     private Logger thislog = Global.getLogger(this.getClass());
     SectorAPI sector;
     CampaignUIAPI cUI;
+    InteractionDialogAPI intDialog;
     @Override
     public boolean isDone() {
         return false;
@@ -42,81 +51,88 @@ public class EveryCampaignFrameScript implements EveryFrameScript {
                 lastKeyState.put(key, false);
         }
 
-        sector = Global.getSector();
-        cUI = sector.getCampaignUI();
+        boolean aKeyPressed = false;
 
-        String cUITabName = null;
-        try {cUITabName = cUI.getCurrentCoreTab().name();} catch (Exception e) { }
+        for (int key : campaignListeningToKeys)
+            if (Keyboard.isKeyDown(key))
+                aKeyPressed = true;
 
-        if (cUITabName == null)
+        if (aKeyPressed)
         {
-        } else {
-            if (Keyboard.isKeyDown(CampaignUIRight) && !lastKeyState.get(CampaignUIRight))
+            sector = Global.getSector();
+            cUI = sector.getCampaignUI();
+
+            String cUITabName = null;
+            try {cUITabName = cUI.getCurrentCoreTab().name();} catch (Exception e) { }
+
+            if (cUITabName == null)
             {
-                switch (cUITabName) {
-                    case "CHARACTER":
-                        T1000.keyPress(KeyEvent.VK_F);
-                        T1000.keyRelease(KeyEvent.VK_F);
-                        break;
-                    case "FLEET":
-                        T1000.keyPress(KeyEvent.VK_R);
-                        T1000.keyRelease(KeyEvent.VK_R);
-                        break;
-                    case "REFIT":
-                        T1000.keyPress(KeyEvent.VK_I);
-                        T1000.keyRelease(KeyEvent.VK_I);
-                        break;
-                    case "CARGO":
-                        T1000.keyPress(KeyEvent.VK_TAB);
-                        T1000.keyRelease(KeyEvent.VK_TAB);
-                        break;
-                    case "MAP":
-                        T1000.keyPress(KeyEvent.VK_E);
-                        T1000.keyRelease(KeyEvent.VK_E);
-                        break;
-                    case "INTEL":
-                        T1000.keyPress(KeyEvent.VK_D);
-                        T1000.keyRelease(KeyEvent.VK_D);
-                        break;
-                    case "OUTPOSTS":
-                        T1000.keyPress(KeyEvent.VK_C);
-                        T1000.keyRelease(KeyEvent.VK_C);
-                        break;
+                List options;
+                if (intDialog != cUI.getCurrentInteractionDialog())
+                {
+                    intDialog = cUI.getCurrentInteractionDialog();
+                    if (intDialog == null)
+                        thislog.info("Cleared interaction");
+                    else {
+                        thislog.info("Updated interaction");
+                    }
                 }
-            } else if (Keyboard.isKeyDown(CampaignUILeft) && !lastKeyState.get(CampaignUILeft))
-            {
-                switch (cUITabName) {
-                    case "CHARACTER":
-                        T1000.keyPress(KeyEvent.VK_D);
-                        T1000.keyRelease(KeyEvent.VK_D);
-                        break;
-                    case "FLEET":
-                        T1000.keyPress(KeyEvent.VK_C);
-                        T1000.keyRelease(KeyEvent.VK_C);
-                        break;
-                    case "REFIT":
-                        T1000.keyPress(KeyEvent.VK_F);
-                        T1000.keyRelease(KeyEvent.VK_F);
-                        break;
-                    case "CARGO":
-                        T1000.keyPress(KeyEvent.VK_R);
-                        T1000.keyRelease(KeyEvent.VK_R);
-                        break;
-                    case "MAP":
-                        T1000.keyPress(KeyEvent.VK_I);
-                        T1000.keyRelease(KeyEvent.VK_I);
-                        break;
-                    case "INTEL":
-                        T1000.keyPress(KeyEvent.VK_TAB);
-                        T1000.keyRelease(KeyEvent.VK_TAB);
-                        break;
-                    case "OUTPOSTS":
-                        T1000.keyPress(KeyEvent.VK_E);
-                        T1000.keyRelease(KeyEvent.VK_E);
-                        break;
+
+            } else {
+                if (Keyboard.isKeyDown(CampaignUIRight) && !lastKeyState.get(CampaignUIRight))
+                {
+                    switch (cUITabName) {
+                        case "CHARACTER":
+                            cUI.showCoreUITab(CoreUITabId.FLEET);
+                            break;
+                        case "FLEET":
+                            cUI.showCoreUITab(CoreUITabId.REFIT);
+                            break;
+                        case "REFIT":
+                            cUI.showCoreUITab(CoreUITabId.CARGO);
+                            break;
+                        case "CARGO":
+                            cUI.showCoreUITab(CoreUITabId.MAP);
+                            break;
+                        case "MAP":
+                            cUI.showCoreUITab(CoreUITabId.INTEL);
+                            break;
+                        case "INTEL":
+                            cUI.showCoreUITab(CoreUITabId.OUTPOSTS);
+                            break;
+                        case "OUTPOSTS":
+                            cUI.showCoreUITab(CoreUITabId.CHARACTER);
+                            break;
+                    }
+                } else if (Keyboard.isKeyDown(CampaignUILeft) && !lastKeyState.get(CampaignUILeft))
+                {
+                    switch (cUITabName) {
+                        case "CHARACTER":
+                            cUI.showCoreUITab(CoreUITabId.OUTPOSTS);
+                            break;
+                        case "FLEET":
+                            cUI.showCoreUITab(CoreUITabId.CHARACTER);
+                            break;
+                        case "REFIT":
+                            cUI.showCoreUITab(CoreUITabId.FLEET);
+                            break;
+                        case "CARGO":
+                            cUI.showCoreUITab(CoreUITabId.REFIT);
+                            break;
+                        case "MAP":
+                            cUI.showCoreUITab(CoreUITabId.CARGO);
+                            break;
+                        case "INTEL":
+                            cUI.showCoreUITab(CoreUITabId.MAP);
+                            break;
+                        case "OUTPOSTS":
+                            cUI.showCoreUITab(CoreUITabId.INTEL);
+                            break;
+                    }
                 }
             }
         }
+
 
 
 
