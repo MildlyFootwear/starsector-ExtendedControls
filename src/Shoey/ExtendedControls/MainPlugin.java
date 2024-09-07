368e274617b3967d6336a88ef4e2fbad0e1d112e
Shoey <shoeyfiller@gmail.com>
2024-09-07 08:15:24 -0400

    cleaning up warnings

--- src/Shoey/ExtendedControls/MainPlugin.java
+++ src/Shoey/ExtendedControls/MainPlugin.java
@@ -4,7 +4,6 @@ import com.fs.starfarer.api.BaseModPlugin;
 import com.fs.starfarer.api.campaign.CampaignUIAPI;
 import com.fs.starfarer.api.campaign.InteractionDialogAPI;
 import com.fs.starfarer.api.campaign.SectorAPI;
-import com.fs.starfarer.api.util.Misc;
 import lunalib.lunaSettings.LunaSettings;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -64,25 +63,25 @@ public class MainPlugin extends BaseModPlugin {
     
     public static void updateLunaSettings()
     {
-        debugLogging = LunaSettings.getBoolean("ShoeyExtendedControls", "Debugging");
+        debugLogging = Boolean.TRUE.equals(LunaSettings.getBoolean("ShoeyExtendedControls", "Debugging"));
         campaignListeningToKeys.clear();
         CampaignCoreUILeft = putCampaignBind("CampaignUILeft");
         CampaignCoreUIRight = putCampaignBind("CampaignUIRight");
 
-        HandlingInteract = LunaSettings.getBoolean("ShoeyExtendedControls", "HandlingInteract");
+        HandlingInteract = Boolean.TRUE.equals(LunaSettings.getBoolean("ShoeyExtendedControls", "HandlingInteract"));
         CampaignInteractUIUp = putCampaignBind("CampaignInteractUIUp");
         CampaignInteractUIDown = putCampaignBind("CampaignInteractUIDown");
         CampaignInteractUIConfirm = putCampaignBind("CampaignInteractUIConfirm");
         CampaignInteractUIToggleIndicator = putCampaignBind("CampaignInteractUIToggleIndicator");
         CampaignInteractUIIndicatorColor = LunaSettings.getColor("ShoeyExtendedControls", "CampaignInteractUIIndicatorColor");
 
-        HandlingHotbar = LunaSettings.getBoolean("ShoeyExtendedControls", "HandlingHotbar");
+        HandlingHotbar = Boolean.TRUE.equals(LunaSettings.getBoolean("ShoeyExtendedControls", "HandlingHotbar"));
         CampaignHotbarLeft = putCampaignBind("CampaignHotbarLeft");
         CampaignHotbarRight = putCampaignBind("CampaignHotbarRight");
         CampaignHotbarConfirm = putCampaignBind("CampaignHotbarConfirm");
-        CampaignHotbarFadeEnabled = LunaSettings.getBoolean("ShoeyExtendedControls", "CampaignHotbarFadeEnabled");
+        CampaignHotbarFadeEnabled = Boolean.TRUE.equals(LunaSettings.getBoolean("ShoeyExtendedControls", "CampaignHotbarFadeEnabled"));
         CampaignHotbarFadeTimer = LunaSettings.getInt("ShoeyExtendedControls", "CampaignHotbarFadeTimer");
-        CampaignHotbarFadeReset = LunaSettings.getBoolean("ShoeyExtendedControls", "CampaignHotbarFadeReset");
+        CampaignHotbarFadeReset = Boolean.TRUE.equals(LunaSettings.getBoolean("ShoeyExtendedControls", "CampaignHotbarFadeReset"));
         CampaignHotbarIndicatorColor = LunaSettings.getColor("ShoeyExtendedControls", "CampaignHotbarIndicatorColor");
 
     }
